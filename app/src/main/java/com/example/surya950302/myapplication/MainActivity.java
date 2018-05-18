55e1939e8c6eea3face310e5f92201c51f061d4c
surya <surya950302@gmail.com>
2018-05-17 22:00:07 -0400

    test

--- app/src/main/java/com/example/surya950302/myapplication/MainActivity.java
+++ app/src/main/java/com/example/surya950302/myapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //hello
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
         setSupportActionBar(mToolbar);
